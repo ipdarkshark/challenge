c520b9a9f4ec52c72ac67f8cfbb8fe8ac1603a41
Ivan Pozdnov <arr.slice@gmail.com>
2017-12-22 07:27:53 +0200

    working version

--- index.js
+++ index.js
@@ -1,72 +1,60 @@
+const switchers = [...document.querySelectorAll('.input-switch input[type="checkbox"]')];
 const socket = new WebSocket('ws://switch.darkshark.pro');
-const levers = [...document.querySelectorAll('.input-switch input[type="checkbox"]')];
 
-const state = {
-  levers: [false, false, false, false],
-  stateId: null,
-}
+const levers = [false, false, false, false]
+const knownLever = 0;
+let checkedLever = 1;
+let stateFlag = true;
 
-let knownLever;
-let checkingLever;
-let ifAllSameFlag = true;
-
-socket.onopen = () => console.log('Connected');
-socket.onmessage = event => {
-  const data = JSON.parse(event.data);
-
-  if (data.pulled >= 0) {
-    updateState(data.pulled, data.stateId);  
-    checkingLever = knownLever === state.levers.length - 1 ? knownLever - 1 : knownLever + 1;
-    sendQuery(knownLever, checkingLever, state.stateId);
-    console.log('state', state.levers);
-    if (checkAllSame()) turnOff(state.stateId);
-  }
-
-  if (data.same) {
-    updateStateAfterCheck(knownLever, checkingLever);
-  }
-
-  if (data.newState === 'poweredOn') {
-    ifAllSameFlag = false;
-  }
-  else if (data.newState === 'poweredOff') {
-    socket.close();
-    console.log('Token: ', data.token);
-  }
-}
-
-const updateState = (pulledLever, stateId) => {
-  state.levers[pulledLever] = !state.levers[pulledLever];
-  state.stateId = stateId;
-  knownLever = pulledLever;
-  toggleLever();
-}
-const updateStateAfterCheck = (pulledLever, checkedLever) => {
-  state.levers[checkedLever] = checkedLever !== pulledLever ? state.levers[pulledLever] : state.levers[checkedLever];
-}
-
-const toggleLever = () => {
-  levers.forEach((lever, i) => lever.checked = state.levers[i])
-}
-
-const checkAllSame = () => {
-  return state.levers.every( lever => lever === ifAllSameFlag );
-}
-
-const sendQuery = (lever1, lever2, stateId) => {
+const sendCompareQuery = (lever1, lever2, stateId) => {
   const query = {
     action: 'check',
     lever1,
     lever2,
     stateId,
   }
+
   socket.send(JSON.stringify(query));
 }
 
-const turnOff = stateId => {
+const sendTurnOffQuery = stateId => {
   const query = {
     action: 'powerOff',
     stateId,
   }
+
   socket.send(JSON.stringify(query));
+}
+
+const checkAllSameOrNot = () => levers.every( lever => lever === stateFlag );
+
+const toggleLevers = () => {
+  switchers.forEach((switcher, i) => {
+    switcher.checked = levers[i]
+  })
+}
+
+socket.onopen = () => console.log('Connected');
+socket.onclose = () => console.log('Disconnected');
+socket.onmessage = event => {
+  const data = JSON.parse(event.data);
+
+  if (data.pulled >= 0) {
+    levers[data.pulled] = !levers[data.pulled];
+    toggleLevers();
+    sendCompareQuery(knownLever, checkedLever, data.stateId);
+  }
+
+  if (data.newState === 'poweredOn') stateFlag = false;
+
+  if (data.newState === 'poweredOff') {
+    console.log('Token: ', data.token);
+    socket.close();
+  }
+
+  if (data.same) {
+    levers[checkedLever] = levers[knownLever];
+    if (checkedLever < levers.length -1 ) checkedLever++;
+    if (checkAllSameOrNot()) sendTurnOffQuery(data.stateId);
+  }
 }
